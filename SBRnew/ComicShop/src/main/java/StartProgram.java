511450b14e25f378bd39bc7fc403c4d5432df9b4
Ghost-Rog <ghostrog@mail.ru>
2020-10-26 17:19:39 +0300

    ComicShop

---
+++ SBRnew/ComicShop/src/main/java/StartProgram.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class StartProgram {
+
+
+
+    static void start(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите логин: ");
+        String login = in.next();
+        System.out.println("Введите пароль: ");
+        String password = in.next();
+        PersonDB personDB = new PersonDB();
+        if(login.equals(login)){
+            ComicBase comicBase = new ComicBase();
+            Discount discount = new Discount();
+            comicBase.comicFactory();
+            AdminMenu.printAdminMenu(login, comicBase, discount);
+        }
+
+    }
+
+}
