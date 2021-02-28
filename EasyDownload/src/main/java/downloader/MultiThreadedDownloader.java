b0e5a82608b61089fb0b9fc1761a4053eb5fb5dc
RomanManokhin <70842828+RomanManokhin@users.noreply.github.com>
2021-02-28 18:30:19 +0300

    Delete EasyDownload directory

--- EasyDownload/src/main/java/downloader/MultiThreadedDownloader.java
+++
@@ -1,9 +0,0 @@
-package downloader;
-
-import java.util.List;
-
-public interface MultiThreadedDownloader {
-
-    void startDownloading(int countThreads, int countUrls, List<String> urls, List<String> fileNames);
-
-}
